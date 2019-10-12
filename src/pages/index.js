cdbe181181958714cbe4cb248ccf198d02402296
Tim Hwang <timothy@nash.io>
2019-10-12 18:45:54 -0400

    feat(Index): Add initial shape decorations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Layout from 'components/layout';
 import Box from 'components/box';
 import Title from 'components/title';
-import Gallery from 'components/gallery';
-import IOExample from 'components/io-example';
-import Modal from 'containers/modal';
-import { graphql } from 'gatsby';
+import Text from 'components/text';
+import SplashShapeTop from 'images/splash-shape-top.svg';
+import SplashShapeBottom from 'images/splash-shape-bottom.svg';
 
-const Index = ({ data }) => (
+const Index = () => (
   <Layout>
-    <Box>
-      <Title as="h2" size="large">
+    <Box
+      minHeight="480px"
+      maxWidth="960px"
+      display="flex"
+      flexDirection="column"
+      padding="0 4rem"
+    >
+      <SplashShapeTop
+        style={{
+          position: 'absolute',
+          top: '-50px',
+          left: '-50px',
+          zIndex: -1,
+        }}
+      />
+      <Title as="h2" size="large" color="Whites.100">
         Where tech happens in New Haven.
       </Title>
+      <Text as="p" color="Whites.100">
+        NewHaven.io is a tech community lorem ipsum dolor sit amet, consectetur
+        adipiscing elit.
+      </Text>
     </Box>
-
-    <div style={{ height: '50vh' }} />
-
+    <Box minHeight="300px" padding="4rem">
+      <Title as="h2" size="large" color="Grays.100">
+        Join the Slack Community
+      </Title>
+    </Box>
+    <Box
+      minHeight="300px"
+      padding="4rem"
+      position="relative"
+      display="flex"
+      flexDirection="column"
+      alignItems="flex-end"
+      overflow="hidden"
+    >
+      <SplashShapeBottom
+        style={{
+          position: 'absolute',
+          top: '-50px',
+          right: '-50px',
+          zIndex: -1,
+        }}
+      />
+      <Title as="h2" size="large" color="Grays.100">
+        Meet People IRL
+      </Title>
+    </Box>
   </Layout>
 );
 
@@ -26,28 +67,3 @@ Index.propTypes = {
 };
 
 export default Index;
-
-export const query = graphql`
-  query HomepageQuery {
-    homeJson {
-      title
-      content {
-        childMarkdownRemark {
-          html
-          rawMarkdownBody
-        }
-      }
-      gallery {
-        title
-        copy
-        image {
-          childImageSharp {
-            fluid(maxHeight: 500, quality: 90) {
-              ...GatsbyImageSharpFluid_withWebp
-            }
-          }
-        }
-      }
-    }
-  }
-`;
